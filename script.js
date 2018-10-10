f9fa2efed91b426180a0f83749cd1c56d567146f
khoursaaf <khoursa@gmail.com>
2018-10-10 11:04:02 +0200

    firqt commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function khoursa() {
+  var lastname = document.getElementById("lastname").value="";
+      firstname = document.getElementById('firstname').value="";
+      city = document.getElementById('city').value="";
+}
